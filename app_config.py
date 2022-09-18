617dc5d12147a9bdf4e4859c2033c7ef25247381
JustinL-12 <jlong30@uic.edu>
2022-09-18 16:13:24 -0500

    Now logs date, result, and early surrender

--- app_config.py
+++ app_config.py
@@ -1,1 +1,4 @@
 api_key = 'RGAPI-6fb8f017-451b-4238-837d-da72479c49cd'
+
+# lol summoner info
+summoner_id = 'Quigls'
--- main.py
+++ main.py
@@ -1,21 +1,44 @@
 import app_config
 import requests
 import json
+import datetime
+
+
+def get_request(url):
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        raise Exception("GET request failed with status code " + str(resp.status_code))
+    resp_json = resp.json()
+    return resp_json
+
 
 if __name__ == '__main__':
-    # Create request url
-    url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/YungNutz?api_key=" + app_config.api_key
+    # Get puuid by summoner id
+    url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/+" + app_config.summoner_id + "?api_key=" + app_config.api_key
+    puuid = get_request(url)['puuid']
 
-    # Submit GET request
-    resp = requests.get(url)
+    # Get most recent aram game
+    url = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?queue=450&api_key=" + app_config.api_key
+    game_id = get_request(url)[0]
 
-    # Check status of GET request
-    if resp.status_code != 200:
-        raise Exception("GET  request failed with status code " + resp.status_code)
+    # Get aram game info
+    url = "https://americas.api.riotgames.com/lol/match/v5/matches/" + game_id + "?api_key=" + app_config.api_key
+    game_info = get_request(url)
+    teams = game_info['info']['teams']
+    participants = game_info['info']['participants']
 
-    # Print response
-    resp_json = resp.json()
-    print(resp_json)
+    # Determine which team is ours
+    for p in participants:
+        if p['puuid'] == puuid:
+            team_id = p['teamId']
+            early_surrender = p['teamEarlySurrendered']
+            break
+    for t in teams:
+        if t['teamId'] == team_id:
+            team = t
+            break
+    result = 'Win' if team['win'] else 'Lose'
 
-
-
+    # Build logging structure
+    logging_info = [str(datetime.date.today()), result, early_surrender]
+    print(logging_info)
