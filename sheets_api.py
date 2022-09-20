f19b9f480ac00e24b790728e6f8392c4260a5268
Quigs11 <quigleyconner@gmail.com>
2022-09-20 13:38:22 -0500

    Start on Google Sheets API and OAuth files

--- sheets_api.py
+++ sheets_api.py
@@ -1,2 +1,18 @@
+"""
+pip install --upgrade google-api-client google-auth-httplib2 google-auth-oauthlib
+"""
+import os
+from Google import Create_Service
 import app_config
 import lol_api
+
+CLIENT_SECRET_FILE = 'LeagueTrackerGoogle.json'
+API_NAME = 'sheets'
+API_VERSION = 'v4'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+sheets_data = lol_api.pull_league_data()
+
+print(sheets_data[3][0])
