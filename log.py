3f7b9e24c17699a5dbbd7255901bc8158a4b7d8b
FORGEJackson <josh.forge.jackson@gmail.com>
2022-09-24 17:50:11 -0400

    Updated writeLog function
    Updated writeLog function to allow passing custom messages.

--- log.py
+++ log.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-def writeLog(gameID):
+def writeLog(gameID, message):
     lines = []
 
     #Get current file contents (Using with open(...) as ... automatically closes the file even when throwing errors)
@@ -13,11 +13,11 @@ def writeLog(gameID):
 
     #Append data to log
     with open("LeagueTracker.log", "a") as logfile:
-        if len(lines) >= 10:
+        if len(lines) >= 25:
             del lines[0]
 
          #Log current time and most recent game ID
-        newline = datetime.now().strftime(f"%Y-%m-%d, %H:%M:%S -- Most recent: {gameID} -- LeagueTracker/main.py executed successfully...\n")
+        newline = datetime.now().strftime(f"%Y-%m-%d, %H:%M:%S -- gameID: {gameID} -- {message}\n")
         lines.append(newline)
 
         for line in lines:
